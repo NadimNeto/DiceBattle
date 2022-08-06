16d227ecf3cad0fd4c7b6ea50391ab1e24545059
Nadim Neto <70706193+NadimNeto@users.noreply.github.com>
2022-08-06 12:26:26 -0300

    Game-loop finalizado

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 "use strict";
 
 //Selecionando elementos
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 // const score0 = document.querySelector("#score--0");
 // const score1 = document.querySelector("#score--1");
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
-const current0El = document.getElementById("current--0");
-const current1El = document.getElementById("current--1");
 
 //Condições iniciais
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 //Funcionalidade de rolar o dado
 btnRoll.addEventListener("click", function () {
@@ -30,8 +42,23 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     //Adiciona o valor do dado aos pontos
     currentScore += dice;
-    current0El.textContent = currentScore; //ARRUMAR FUNCIONALIDADE 2 JOGADORES
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    //trocar de jogador
+    //Trocar de jogador
+    switchPlayer();
+  }
+});
+
+//Funcionalidade do botão "Segurar"
+btnHold.addEventListener("click", function () {
+  {
+    //Adicionar pontos aos pontos totais
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //Checar se jogador chegou a 100 pontos: Terminar o jogo, Trocar jogador
+
+    switchPlayer();
   }
 });
